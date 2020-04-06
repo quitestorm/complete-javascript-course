6b49323a7f411a88b53071b44bfa2e4ba1bf4909
quitestorm <Rena.Lynch@my.scitexas.edu>
2020-04-05 22:27:28 -0700

    started

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,25 @@
+alert("hey!");
+
+var firstName = "Rena`"
+var lastName = "Lynch"
+var age = 48
+ 
+function Happy(){
+    
+    firstName + lastName
+}
+alert(firstName + lastName + " is "+ age)
+
+var Dog = prompt("What is your dogs name?");
+alert(Dog)
+
+
+
+var markHeight, johnsHeight, marksMass, johnsMass
+markHeight = 78
+johnsHeight =67
+marksMass = 134
+johnsMass = 164
+
+var BMI = marksMass / (markHeight *2 )
+alert(BMI)
